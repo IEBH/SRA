2fd1a8959af92d16fdc0bb3b0d2778f464612cf5
Matt Carter <m@ttcarter.com>
2013-06-27 15:34:52 +1000

    Added additional fields to reference exporter

--- application/controllers/references.php
+++ application/controllers/references.php
@@ -32,19 +32,26 @@ class References extends CI_Controller {
 			echo '<foreign-keys><key app="EN" db-id="s55prpsswfsepue0xz25pxai2p909xtzszzv">' . $refno . '</key></foreign-keys>';
 			echo '<ref-type name="Journal Article">17</ref-type>';
 
-			// FIXME: Wrong
-			echo '<contributors><authors><author><style face="normal" font="default" size="100%">FAKE AUTHOR</style></author></authors></contributors>';
+			echo '<contributors><authors><author><style face="normal" font="default" size="100%">' . $ref['ref'] . '</style></author></authors></contributors>';
+			echo '<auth-address><style face="normal" font="default" size="100%">FIXME: CONTACT_NAME + CONTACT_EMAIL</style></auth-address>';
 			echo '<titles>';
-			echo '<title><style face="normal" font="default" size="100%">' . $ref['title'] . '</style></title>';
-			echo '<secondary-title><style face="normal" font="default" size="100%">FAKE JOURNAL</style></secondary-title>';
+				echo '<title><style face="normal" font="default" size="100%">' . $ref['title'] . '</style></title>';
+				echo '<secondary-title><style face="normal" font="default" size="100%">' . '</style></secondary-title>';
+				echo '<short-title><style face="normal" font="default" size="100%">' . $ref['title-scientific'] . '</style></short-title>';
 			echo '</titles>';
-			echo '<periodical><full-title><style face="normal" font="default" size="100%">FAKE JOURNAL</style></full-title></periodical>';
-			echo '<pages><style face="normal" font="default" size="100%">FAKE PAGES</style></pages>';
-			echo '<volume><style face="normal" font="default" size="100%">FAKE VOLUME</style></volume>';
-			echo '<number><style face="normal" font="default" size="100%">FAKE ISSUE</style></number>';
-			echo '<section><style face="normal" font="default" size="100%">FAKE StartPage</style></section>';
-			echo '<dates><year><style face="normal" font="default" size="100%">FAKE YEAR</style></year></dates>';
-			echo '<urls/>';
+
+			echo '<periodical><full-title><style face="normal" font="default" size="100%">' . '</style></full-title></periodical>';
+			echo '<pages><style face="normal" font="default" size="100%">' . '</style></pages>';
+			echo '<volume><style face="normal" font="default" size="100%">' . '</style></volume>';
+			echo '<number><style face="normal" font="default" size="100%">' . '</style></number>';
+			echo '<section><style face="normal" font="default" size="100%">' . '</style></section>';
+			echo '<dates>'
+				echo '<year><style face="normal" font="default" size="100%">' . '</style></year>';
+				echo '<pub-dates><date><style face="normal" font="default" size="100%">' . $ref['date-ref'] . '</style></date></pub-dates>';
+			echo '</dates>';
+			echo '<abstract><style face="normal" font="default" size="100%">FIXME: PRIMARY_OUTCOMES</style></abstract>';
+			echo '<urls><related-urls><url><style face="normal" font="default" size="100%">' . $ref['url-real'] . '</style></url></related-urls></urls>';
+			echo '<research-notes><style face="normal" font="default" size="100%">FIXME: STUDY_TYPE + STUDY_DESIGN</style></research-notes>';
 
 			echo '</record>';
 		}
