d29fabe1373e2f1c53eed4e442e1937363e55b33
Matt Carter <m@ttcarter.com>
2013-08-01 12:48:35 +1000

    Converted controllers/references#Export() to use PHP-EndNote instead of the built in XML formatter

--- application/controllers/references.php
+++ application/controllers/references.php
@@ -21,40 +21,22 @@ class References extends CI_Controller {
 		if (!$references = $this->Basket->GetAll())
 			$this->Site->Error('No references to export');
 
-		header('Content-type: text/plain');
-		header('Content-Disposition: attachment; filename="CREB-SearchTool.xml"');
-		echo '<?xml version="1.0" encoding="UTF-8"?><xml><records>';
+		require('lib/php-endnote/endnote.php');
+		$this->endnote = new PHPEndNote();
+
 		foreach ($references as $refno => $ref) {
-			echo '<record>';
-			echo '<database name="CREBP-SearchTool.enl" path="C:\CREBP-SearchTool.enl">CREBP-SearchTool.enl</database>';
-			echo '<source-app name="EndNote" version="16.0">EndNote</source-app>';
-			echo '<rec-number>' . $refno . '</rec-number>';
-			echo '<foreign-keys><key app="EN" db-id="s55prpsswfsepue0xz25pxai2p909xtzszzv">' . $refno . '</key></foreign-keys>';
-			echo '<ref-type name="Journal Article">17</ref-type>';
-
-			echo '<contributors><authors><author><style face="normal" font="default" size="100%">' . $ref['ref'] . '</style></author></authors></contributors>';
-			echo '<auth-address><style face="normal" font="default" size="100%">' . $ref['contact-name'] . ($ref['contact-email'] ? ' - ' . $ref['contact-email'] : '') . '</style></auth-address>';
-			echo '<titles>';
-				echo '<title><style face="normal" font="default" size="100%">' . $ref['title'] . '</style></title>';
-				echo '<secondary-title><style face="normal" font="default" size="100%">' . '</style></secondary-title>';
-				echo '<short-title><style face="normal" font="default" size="100%">' . $ref['title-scientific'] . '</style></short-title>';
-			echo '</titles>';
-
-			echo '<periodical><full-title><style face="normal" font="default" size="100%">' . '</style></full-title></periodical>';
-			echo '<pages><style face="normal" font="default" size="100%">' . '</style></pages>';
-			echo '<volume><style face="normal" font="default" size="100%">' . '</style></volume>';
-			echo '<number><style face="normal" font="default" size="100%">' . '</style></number>';
-			echo '<section><style face="normal" font="default" size="100%">' . '</style></section>';
-			echo '<dates>';
-				echo '<year><style face="normal" font="default" size="100%">' . '</style></year>';
-				echo '<pub-dates><date><style face="normal" font="default" size="100%">' . $ref['date-ref'] . '</style></date></pub-dates>';
-			echo '</dates>';
-			echo '<abstract><style face="normal" font="default" size="100%">' . $ref['primary-outcomes'] . '</style></abstract>';
-			echo '<urls><related-urls><url><style face="normal" font="default" size="100%">' . $ref['url-real'] . '</style></url></related-urls></urls>';
-			echo '<research-notes><style face="normal" font="default" size="100%">STUDY TYPE' . "\n" . $ref['study-type'] . "\n\nSTUDY DESIGN\n" . $ref['study-design'] . '</style></research-notes>';
-
-			echo '</record>';
+			$this->endnote->Add(array(
+				'author' => $ref['ref'],
+				'address' => $ref['contact-name'] . ($ref['contact-email'] ? ' - ' . $ref['contact-email'] : ''),
+				'title' => $ref['title'],
+				'title-short' => $ref['title-scientific'],
+				'date' => $ref['date-reg'],
+				'abstract' => $ref['primary-outcomes'],
+				'url' => $ref['url-real'],
+				'notes' => 'STUDY TYPE' . "\n" . $ref['study-type'] . "\n\nSTUDY DESIGN\n" . $ref['study-design'],
+			));
 		}
-		echo '</records></xml>';
+		//$this->endnote->OutputXML();
+		echo $this->endnote->GetXML();
 	}
 }
