a61abeeb3ca77224f596281159beb9647e453f38
Matt Carter <m@ttcarter.com>
2013-09-05 13:30:59 +1000

    Moved a[data-confirm] functionality into upstream Shoelace library

--- js/core.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-	// Apply [data-confirm] functionality {{{
-	$('a[data-confirm]').click(function(event) {
-		var message = $(this).data('confirm') || 'Are you really sure you wish to do this?';
-		if (!confirm(message))
-			event.preventDefault();
-	});
-	// }}}
-});
