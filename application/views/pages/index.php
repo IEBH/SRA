1e522fa5d54f80732b919681d3131b8e1db5eed9
Matt Carter <m@ttcarter.com>
2013-09-24 16:38:49 +1000

    Changed buttons on front page

--- application/views/pages/index.php
+++ application/views/pages/index.php
@@ -2,10 +2,16 @@
 	<div class="hero-unit">
 		<h1><?=SITE_TITLE?></h1>
 		<p>Welcome to the CREBP Systematic Review Creator</p>
+		<? if ($this->User->GetActive()) { // Logged in ?>
 		<div class="pad-top-huge pull-center">
 			<a class="btn btn-large" href="<?=SITE_ROOT?>how-to"><i class="icon-book"></i> How to create a review</a>
-			<a class="btn btn-large" href="<?=SITE_ROOT?>signup"><i class="icon-asterisk"></i> Create an account</a>
-			<a class="btn btn-large" href="<?=SITE_ROOT?>user"><i class="icon-profile"></i> Login</a>
+			<a class="btn btn-large" href="<?=SITE_ROOT?>libraries"><i class="icon-tags"></i> View your Libraries</a>
 		</div>
+		<? } else { // Not logged in ?>
+		<div class="pad-top-huge pull-center">
+			<a class="btn btn-large" href="<?=SITE_ROOT?>signup"><i class="icon-asterisk"></i> Create an account</a>
+			<a class="btn btn-large" href="<?=SITE_ROOT?>login"><i class="icon-user"></i> Login</a>
+		</div>
+		<? } ?>
 	</div>
 </div>
