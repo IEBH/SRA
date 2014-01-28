23723c13d9c91ca12d6f6fe6b4f07d910de0af7e
Matt Carter <m@ttcarter.com>
2014-01-28 10:59:21 +1000

    Added debug functionality to copy columns from one XML file to another

--- application/controllers/debug.php
+++ application/controllers/debug.php
@@ -63,11 +63,52 @@ class Debug extends CI_Controller {
 		$this->endnote = new PHPEndNote();
 		$this->endnote->SetXMLFile('data/John/131120 TEST IMPORT.xml');
 		foreach ($this->endnote->refs as $refno => $ref) {
-			if (isset($myrefs[$ref['caption']]))
-				$ref['language'] = $ref['label'];
+			if (isset($myrefs[$ref['caption']])) {
+				switch ($myrefs[$ref['caption']]['status']) {
+					case 'active':
+						$ref['language'] = 'OK';
+						break;
+					case 'dupe':
+						$ref['language'] = 'DUPE';
+						break;
+					case 'deleted':
+						$ref['language'] = 'DELETED';
+						break;
+					default:
+						$ref['language'] = 'ERROR';
+				}
+			}
 			$this->endnote->refs[$refno] = $ref; // Save back
 		}
 		$this->endnote->OutputXML();
+	}
+
+	
+	/**
+	* Copies the field 'authors' from one XML file to another and returns the output as a third
+	*/
+	function BackportCopy() {
+		$this->load->model('Reference');
+		require('lib/php-endnote/endnote.php');
+	
+		$this->source = new PHPEndNote();
+		$this->source->refId = 'caption';
+		$this->source->SetXMLFile('data/John/A057 - Post SRA Friday 27-11-2013.xml');
+
+		$this->dest = new PHPEndNote();
+		$this->dest->refId = 'caption';
+		$this->dest->SetXMLFile('data/John/131120 TEST IMPORT-OUT.xml');
+
+		foreach ($this->dest->refs as $refid => $ref) {
+			if (isset($this->source->refs[$refid])) {
+				if (isset($this->source->refs[$refid]['authors']))
+					$this->dest->refs[$refid]['authors'] = $this->source->refs[$refid]['authors'];
+			} else {
+				die("Exists in Dest but not in Source: $refid");
+			}
+		}
+
+		$this->dest->OutputXML();
 	}
 }
 ?>
