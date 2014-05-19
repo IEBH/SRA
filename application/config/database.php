63c1e0f5841b95d5a23c7b1259cebbb58a8ab8bc
Matt Carter <m@ttcarter.com>
2014-05-20 09:32:23 +1000

    Added new database connection data

--- application/config/database.php
+++ application/config/database.php
@@ -2,11 +2,16 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-if (preg_match('/crebp\.net\.au/', $_SERVER['SERVER_NAME'])) {
+if (preg_match('/crebp\.net\.au$/', $_SERVER['SERVER_NAME'])) {
 	$db['default']['hostname'] = 'localhost';
 	$db['default']['database'] = 'cre21534_sra';
 	$db['default']['username'] = 'cre21534_sra';
 	$db['default']['password'] = 'u35K9gxDmpdw';
+} elseif (preg_match('/crebp-sra\.com$/', $_SERVER['SERVER_NAME'])) {
+	$db['default']['hostname'] = 'localhost';
+	$db['default']['database'] = 'crebp_sra';
+	$db['default']['username'] = 'crebp_sra';
+	$db['default']['password'] = 'moojiew7Otue6eo';
 } else {
 	$db['default']['hostname'] = 'localhost';
 	$db['default']['database'] = 'sra';
