afd11651b1c275dcd27f35129431e1f8c5c2cda9
Matt Carter <m@ttcarter.com>
2014-07-10 16:25:18 +1000

    Upgraded Joyst to latest version

--- vendor/hashbang/joyst/Joyst_Model.php
+++ vendor/hashbang/joyst/Joyst_Model.php
@@ -486,11 +486,11 @@ class Joyst_Model extends CI_Model {
 		if (!$this->continue)
 			return FALSE;
 
-		$this->Trigger('access', $row);
+		$this->Trigger('access', $this->query['where']);
 		if (!$this->continue)
 			return FALSE;
 
-		$this->Trigger('pull', $row);
+		$this->Trigger('pull', $this->query['where']);
 		if (!$this->continue)
 			return FALSE;
 
@@ -871,8 +871,8 @@ class Joyst_Model extends CI_Model {
 
 		if ($this->enforceTypes)
 			foreach ($this->schema as $key => $props)
-				if (isset($data[$key]))
-					$data[$key] = $this->UnCastType($props['type'], $data[$key], $data);
+				if (isset($data[$key]) || $props['type'] == 'json-import')
+					$data[$key] = $this->UnCastType($props['type'], isset($data[$key]) ? $data[$key] : null, $data);
 
 		$this->ResetQuery(array(
 			'method' => 'create',
@@ -928,8 +928,8 @@ class Joyst_Model extends CI_Model {
 
 		if ($this->enforceTypes)
 			foreach ($this->schema as $key => $props)
-				if (isset($data[$key]))
-					$data[$key] = $this->UnCastType($props['type'], $data[$key], $data);
+				if (isset($data[$key]) || $props['type'] == 'json-import')
+					$data[$key] = $this->UnCastType($props['type'], isset($data[$key]) ? $data[$key] : null, $data);
 
 		$this->ResetQuery(array(
 			'method' => 'save',
